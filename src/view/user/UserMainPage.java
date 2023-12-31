3ddb27ce256605c1edfdced618ef1f7760623303
Wallxtr41 <huseyin.acemli@ozu.edu.tr>
2023-12-31 18:27:12 +0300

    home,logout, exit and refresh buttons obtain functionality and variable names is more clear.

--- src/view/user/UserMainPage.java
+++ src/view/user/UserMainPage.java
@@ -1,6 +1,10 @@
 package view.user;
 
 
+import controller.DatabaseConnection;
+import controller.EventController;
+import view.LoginPage;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -32,10 +36,11 @@ import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 import javax.swing.JList;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
-
+import model.*;
 public class UserMainPage extends JFrame{
 
     private JFrame frame;
@@ -73,78 +78,75 @@ public class UserMainPage extends JFrame{
         frame.getContentPane().setLayout(new BorderLayout(0, 0));
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 
-        JPanel panel = new JPanel();
-        frame.getContentPane().add(panel, BorderLayout.NORTH);
-        panel.setLayout(new GridLayout(1, 0, 0, 0));
+        JPanel menuPanel = new JPanel();
+        frame.getContentPane().add(menuPanel, BorderLayout.NORTH);
+        menuPanel.setLayout(new GridLayout(1, 0, 0, 0));
 
-        JPanel panel_2 = new JPanel();
-        panel.add(panel_2);
-        panel_2.setLayout(new GridLayout(0, 4, 0, 0));
+        JPanel menuButtonPanel = new JPanel();
+        menuPanel.add(menuButtonPanel);
+        menuButtonPanel.setLayout(new GridLayout(0, 5, 0, 0));
 
-        JButton btnNewButton = new JButton("Home");
-        panel_2.add(btnNewButton);
+        JButton homeButton = new JButton("Home");
+        homeButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                frame.dispose();
+                UserMainPage userMainPage = new UserMainPage();
+            }
+        });
+        menuButtonPanel.add(homeButton);
 
-        JButton btnNewButton_1 = new JButton("History");
-        panel_2.add(btnNewButton_1);
+        JButton historyButton = new JButton("History");
+        menuButtonPanel.add(historyButton);
 
-        JButton btnNewButton_2 = new JButton("Profile");
-        panel_2.add(btnNewButton_2);
+        JButton profileButton = new JButton("Profile");
+        menuButtonPanel.add(profileButton);
 
-        JButton btnNewButton_3 = new JButton("Logout");
-        panel_2.add(btnNewButton_3);
+        JButton logoutButton = new JButton("Logout");
 
-        JPanel panel_1 = new JPanel();
-        panel.add(panel_1);
-        panel_1.setLayout(new GridLayout(1,0, 0, 0));
-
-        JLabel lblNewLabel_1 = new JLabel("Hello Hüseyin Acemli");
-        lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-        panel_1.add(lblNewLabel_1);
-
-        JLabel lblNewLabel = new JLabel("Balance");
-        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        panel_1.add(lblNewLabel);
-
-
-
-
-
+        logoutButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                frame.dispose();
+                LoginPage Lwindow = new LoginPage();
+            }
+        });
+        menuButtonPanel.add(logoutButton);
 
 
-        String data[][] = {{"001","vinod","Bihar","India","Biology","65","First"},
-                {"002","Raju","ABC","Kanada","Geography","58","second"},
-                {"003","Aman","Delhi","India","computer","98","Dictontion"},
-                {"004","Ranjan","Bangloor","India","chemestry","90","Dictontion"},
-                {"001","vinod","Bihar","India","Biology","65","First"},
-                {"002","Raju","ABC","Kanada","Geography","58","second"},
-                {"003","Aman","Delhi","India","computer","98","Dictontion"},
-                {"004","Ranjan","Bangloor","India","chemestry","90","Dictontion"},
-                {"001","vinod","Bihar","India","Biology","65","First"},
-                {"002","Raju","ABC","Kanada","Geography","58","second"},
-                {"003","Aman","Delhi","India","computer","98","Dictontion"},
-                {"004","Ranjan","Bangloor","India","chemestry","90","Dictontion"},
-                {"001","vinod","Bihar","India","Biology","65","First"},
-                {"002","Raju","ABC","Kanada","Geography","58","second"},
-                {"003","Aman","Delhi","India","computer","98","Dictontion"},
-                {"004","Ranjan","Bangloor","India","chemestry","90","Dictontion"},
-                {"001","vinod","Bihar","India","Biology","65","First"},
-                {"002","Raju","ABC","Kanada","Geography","58","second"},
-                {"003","Aman","Delhi","India","computer","98","Dictontion"},
-                {"004","Ranjan","Bangloor","India","chemestry","90","Dictontion"},
-                {"001","vinod","Bihar","India","Biology","65","First"},
-                {"002","Raju","ABC","Kanada","Geography","58","second"},
-                {"003","Aman","Delhi","India","computer","98","Dictontion"},
-                {"004","Ranjan","Bangloor","India","chemestry","90","Dictontion"},
-                {"001","vinod","Bihar","India","Biology","65","First"},
-                {"002","Raju","ABC","Kanada","Geography","58","second"},
-                {"003","Aman","Delhi","India","computer","98","Dictontion"},
-                {"004","Ranjan","Bangloor","India","chemestry","90","Dictontion"},
-                {"001","vinod","Bihar","India","Biology","65","First"},
-                {"002","Raju","ABC","Kanada","Geography","58","second"},
-                {"003","Aman","Delhi","India","computer","98","Dictontion"},
-                {"004","Ranjan","Bangloor","India","chemestry","90","Dictontion"}};
-        String col[] = {"Roll","Name","State","country","Math","Marks","Grade"};
-        DefaultTableModel model = new DefaultTableModel(data, col) {
+
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                // user status must be changed
+                System.exit(0);
+            }
+        });
+        menuButtonPanel.add(exitButton);
+
+        JPanel menuTextPanel = new JPanel();
+        menuPanel.add(menuTextPanel);
+        menuTextPanel.setLayout(new GridLayout(1,0, 0, 0));
+
+        JLabel helloLabel = new JLabel("Hello Hüseyin Acemli");
+        helloLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        menuTextPanel.add(helloLabel);
+
+        JLabel balanceLabel = new JLabel("Balance");
+        balanceLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        menuTextPanel.add(balanceLabel);
+
+
+
+
+
+
+
+
+
+        DefaultTableModel model = new DefaultTableModel( ) {
             /**
              *
              */
@@ -158,10 +160,24 @@ public class UserMainPage extends JFrame{
 
 
 
+        //Controller part
+        EventController eventController = new EventController(new DatabaseConnection().getConnection()) ;
+        ArrayList<Event> upcomingEventList =   eventController.getUpcomingEvents();
+        String col[] = {"ID","Name","Organizer","Date","Location","Capacity","Sold"};
+        for (String colName: col){
+            model.addColumn(colName);
+        }
+        for (Event event : upcomingEventList){
+            Object [] rowData = {event.getId(),event.getName(),event.getOrganizerUsername(),event.getDate(),event.getLocation(),event.getCapacity(),event.getSold()};
+            model.addRow(rowData);
+        }
 
 
-        JPanel panelTable = new JPanel();
-        frame.getContentPane().add(panelTable, BorderLayout.CENTER);
+
+
+
+        JPanel tablePanel = new JPanel();
+        frame.getContentPane().add(tablePanel, BorderLayout.CENTER);
 
         JTable table = new JTable(model);
         table.setFocusable(false);
@@ -189,20 +205,38 @@ public class UserMainPage extends JFrame{
         //create scroll item
         JScrollPane pane = new JScrollPane(table);
 
-        panelTable.add(pane);
+        tablePanel.add(pane);
 
 
 
         JPanel panelButton = new JPanel();
 
-        panelTable.add(panelButton);
+        tablePanel.add(panelButton);
         panelButton.setLayout(new BoxLayout(panelButton, BoxLayout.Y_AXIS));
 
-        JButton btnNewButton_4 = new JButton("Book Ticket");
-        panelButton.add(btnNewButton_4);
+        JButton bookingButton = new JButton("Book Ticket");
+        panelButton.add(bookingButton);
+        bookingButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+            }
+        });
+
+
         panelButton.add(Box.createVerticalStrut(10));
-        JButton btnNewButton_5 = new JButton("Refresh Table");
-        panelButton.add(btnNewButton_5);
+        JButton refreshButton = new JButton("Refresh Table");
+        refreshButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                model.setRowCount(0);
+                ArrayList<Event> upcomingEventList =   eventController.getUpcomingEvents();
+                for (Event event : upcomingEventList){
+                    Object [] rowData = {event.getId(),event.getName(),event.getOrganizerUsername(),event.getDate(),event.getLocation(),event.getCapacity(),event.getSold()};
+                    model.addRow(rowData);
+                }
+            }
+        });
+        panelButton.add(refreshButton);
 
         frame.setVisible(true);
 
