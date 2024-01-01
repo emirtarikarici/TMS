7e2d45b5f67d46868941f383245dcfd2e3cef6c5
Emir Tarık Arıcı <emir.arici@ozu.edu.tr>
2024-01-01 23:49:59 +0300

    implement login with (in)valid credentials tests

--- Test/LoginControllerTest.java
+++ Test/LoginControllerTest.java
@@ -1,8 +1,11 @@
 import controller.Controller;
 import controller.DatabaseConnection;
 import controller.LoginController;
+import controller.RegisterController;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 
@@ -23,5 +26,34 @@ public class LoginControllerTest {
     void tearDown() throws Exception {
         Main.dropAllTables(connection);
         connection.close();
+    }
+
+
+    @Test
+    void loginWithValidCredentialsReturnsTrue() throws Exception {
+        String validUsername = "validUser";
+        String validPassword = "validPass";
+        Assertions.assertTrue(loginController.login(validUsername, validPassword));
+    }
+
+    @Test
+    void loginWithInvalidCredentialsReturnsFalse() throws Exception {
+        String invalidUsername = "invalidUser";
+        String invalidPassword = "invalidPass";
+        Assertions.assertFalse(loginController.login(invalidUsername, invalidPassword));
+    }
+
+    @Test
+    void loginWithInvalidUsernameReturnsFalse() throws Exception {
+        String invalidUsername = "invalidUser";
+        String validPassword = "validPass";
+        Assertions.assertFalse(loginController.login(invalidUsername, validPassword));
+    }
+
+    @Test
+    void loginWithInvalidPasswordReturnsFalse() throws Exception {
+        String validUsername = "validUser";
+        String invalidPassword = "invalidPass";
+        Assertions.assertFalse(loginController.login(validUsername, invalidPassword));
     }
 }
